69efa8e38358112063e02cd324acbf924cafe39c
MKRF\natalia.tretyakova <natalia@tretyakova>
2021-02-15 12:20:15 +0300

    add webpack

---
+++ js/modules/modal.js
@@ -0,0 +1,56 @@
+function modal() {
+
+  // Modal 
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal');
+
+  function openModal() {
+
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+
+    // фиксируем страницу, чтобы нельзя было скролить ее
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  };
+
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+
+    document.body.style.overflow = ''; // пустые кавычки, браузер подставляет значение по умолчанию
+  }
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal || e.target.getAttribute('data-close') == '') {
+      closeModal();
+    }
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 50000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
+
+}
+
+module.exports = modal;
